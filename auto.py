cc3c4b66e7e67ec6c3d81d095e259ef695127f03
Allekseev <55903749+Allekseev@users.noreply.github.com>
2023-02-23 13:35:00 +0500

    Create auto.py

---
+++ auto.py
@@ -0,0 +1,135 @@
+import machine
+import random
+
+
+def distance(p1, p2):
+    return min(abs(p1[0] - p2[0]), machine.height - 1 - abs(p1[0] - p2[0])) \
+           + min(abs(p1[1] - p2[1]), machine.width - 1 - abs(p1[1] - p2[1]))
+
+
+class Auto:
+
+    def __init__(self, animal):
+        self.body = animal
+        self.posNode = 0
+        self.matrix = [
+            [((1, 90), 1), ((0, 0), 3), ((0, 0), 2)],
+            [((0, 0), 0)],
+            [((0, 0), 0)],
+            [((0, 0), 4)],
+            [((0, 0), 6), ((0, 0), 5), ((0, 0), 2)],
+            [((0, 0), 4)],
+            [((0, 0), 0)],
+        ]
+        self.point = ()
+        self.old = False
+        self.new = False
+        self.counter = 0
+        self.funcs = (self.transitoin0, self.closestPlantPoint1, self.eatPoint2, self.goPoint3, self.pregnancy4, self.goUp5)
+        self.arrows = (self.simple0, self.foodTest1)
+        self.nodes = [self.funcs[0], self.funcs[4], self.funcs[5], self.funcs[1], self.funcs[0], self.funcs[3], self.funcs[2]]
+
+    def turn(self, dirt, field):
+        self.old = False
+        arrow = 0
+        while True:
+            if arrow >= len(self.matrix[self.posNode]):
+                return self.old
+            #print(self.posNode, arrow, self.body.food)
+            if self.arrows[self.matrix[self.posNode][arrow][0][0]](self.matrix[self.posNode][arrow][0][1]):
+                res = self.nodes[self.matrix[self.posNode][arrow][1]](dirt, field)
+                if res == -1:
+                    arrow += 1
+                elif res == 0:
+                    self.posNode = self.matrix[self.posNode][arrow][1]
+                    arrow = 0
+                else:
+                    self.posNode = self.matrix[self.posNode][arrow][1]
+                    return self.old
+            else:
+                arrow += 1
+
+    def transitoin0(self, dirt, field):
+        return 0
+
+    def closestPlantPoint1(self, dirt, field):
+        resPoint = ()
+        for point in self.body.see:
+            if dirt[point[0]][point[1]] and (dirt[point[0]][point[1]].name == "grass" or dirt[point[0]][point[1]].name == "berry"):
+                if resPoint:
+                    if distance((self.body.x, self.body.y), point) < distance((self.body.x, self.body.y), resPoint):
+                        resPoint = point
+                else:
+                    resPoint = point
+        if resPoint:
+            self.point = resPoint
+            return 0
+        return -1
+
+    def eatPoint2(self, dirt, field):
+        point = dirt[self.point[0]][self.point[1]]
+        if not point or point.name not in self.body.diet:
+            return -1
+        if distance((point.x, point.y), (self.body.x, self.body.y)) < 2:
+            self.old = self.body.eat(point)
+            return 1
+        return -1
+
+    def goPoint3(self, dirt, field):
+        if distance((self.point[0], self.point[1]), (self.body.x, self.body.y)) < 2:
+            return -1
+        steps = self.body.speed
+        while steps and distance((self.point[0], self.point[1]), (self.body.x, self.body.y)) > 1:
+            pos = []
+            d = self.body.x - self.point[0]
+            if d > machine.height//2 or 0 > d > -machine.height//2:
+                pos.append(((self.body.x + 1) % machine.height, self.body.y))
+            elif d:
+                pos.append(((self.body.x - 1) % machine.height, self.body.y))
+            d = self.body.y - self.point[1]
+            if d > machine.width//2 or 0 > d > -machine.width//2:
+                pos.append((self.body.x, (self.body.y + 1) % machine.width))
+            elif d:
+                pos.append((self.body.x, (self.body.y - 1) % machine.width))
+            res = []
+            for point in pos:
+                if not field[point[1]][point[0]]:
+                    res.append(point)
+            if len(res) == 0:
+                if steps == self.body.speed:
+                    return -1
+                else:
+                    return 1
+            point = random.choice(res)
+            self.body.look(point)
+            field[self.body.x][self.body.y] = False
+            self.body.x = point[0]
+            self.body.y = point[1]
+            field[self.body.x][self.body.y] = self.body
+            steps -= 1
+        return 1
+
+    def pregnancy4(self, dirt, field):
+        if self.body.baby or self.body.food < machine.babyCost:
+            return -1
+        self.body.food -= machine.babyCost
+        self.body.baby = 1
+        return 1
+
+    def goUp5(self, dirt, field):
+        if field[(self.body.x + self.body.eyes[0]) % machine.height][(self.body.y + self.body.eyes[1]) % machine.width]:
+            return -1
+        steps = self.body.speed
+        while steps and not field[(self.body.x + self.body.eyes[0]) % machine.height][(self.body.y + self.body.eyes[1]) % machine.width]:
+            field[self.body.x][self.body.y] = False
+            self.body.x = (self.body.x + self.body.eyes[0]) % machine.height
+            self.body.y = (self.body.y + self.body.eyes[1]) % machine.width
+            field[self.body.x][self.body.y] = self.body
+            steps -= 1
+        return 1
+
+    def simple0(self, c):
+        return True
+
+    def foodTest1(self, c):
+        return self.body.food > c
