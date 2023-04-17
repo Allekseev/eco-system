f24409ecda7b51861af280a9fb1bc42bac284f77
Allekseev <55903749+Allekseev@users.noreply.github.com>
2023-04-17 23:27:33 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,28 @@ pygame.font.init()
 font = pygame.font.Font(None, 70)
 font_color = (150, 130, 60)   #150 130 60
 bg = (55, 55, 55)   #80, 40, 110
-height = 700  # высота = y
-weight = 700  # ширина = x
+height = 750  # высота = y
+weight = 750  # ширина = x
 sc = pygame.display.set_mode((weight, height))
 clock = pygame.time.Clock()
 run = True
 control = machine.Control()
 cell = height // machine.height
+speed = 1
 while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                for i in range(100):
+                    control.turn()
+            if event.key == pygame.K_RIGHT:
+                speed += 1
+                print(speed)
+            if event.key == pygame.K_LEFT and speed:
+                speed -= 1
+                print(speed)
     sc.fill(bg)
     for plant in control.plants:
         if plant.name == 'grass':
@@ -21,10 +35,10 @@ while run:
         elif plant.name == 'berry':
             pygame.draw.rect(sc, (100, 0, 0), (plant.x * cell, plant.y * cell, cell, cell))
     for animal in control.animals:
-        pygame.draw.rect(sc, (0, 0, ('berry' in animal.diet) * 200), (animal.x * cell, animal.y * cell, cell, cell))
+        pygame.draw.rect(sc, (animal.rgb[0] * (animal.gen >= 0), animal.rgb[1] * (animal.gen >= 0), animal.rgb[2] * (animal.gen >= 0)), (animal.x * cell, animal.y * cell, cell, cell))
     pygame.display.flip()
     clock.tick(10)
-    control.turn()
-    # 40 * (animal.age // 100)
+    for i in range(speed):
+        control.turn()
 
 pygame.quit()
